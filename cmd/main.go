6d570b88152e97af4c63eecc9e993dfdb9179658
Fabrice <schaubfabrice@gmail.com>
2025-02-17 09:50:09 +0100

    404 handler

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,14 @@ func main() {
 	app.Use(middleware.ReqLog())
 	app.Use(middleware.InjectItem("db", db))
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to the homepage!")
+	})
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(404).SendString("Oops! That page doesn't exist. Try again!")
+	})
+
 	app_logger.Info("Starting server on %s", cfg.Host)
 	app.Listen(cfg.Host)
 }
